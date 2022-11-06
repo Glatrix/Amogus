1671286f51448d67b9f59fbd0a620419f0689c6f
Glatrix <73367967+Glatrix@users.noreply.github.com>
2022-11-06 11:45:43 -0600

    Add files via upload

---
+++ AmogusMod/MainMod.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+
+namespace AmogusMod
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            
+        }
+
+        public override void OnGUI()
+        {
+            if(GUI.Button(new Rect(50,50,200,20),"Kill Self"))
+            {
+                PlayerControl.LocalPlayer?.Die(DeathReason.Kill);
+            }
+            if (GUI.Button(new Rect(50, 75, 200, 20), "Unlock All"))
+            {
+                foreach (var csmtc in HatManager.Instance.allPets) { csmtc.Free = true; }
+                foreach (var csmtc in HatManager.Instance.allSkins) { csmtc.Free = true; }
+                foreach (var csmtc in HatManager.Instance.allVisors) { csmtc.Free = true; }
+                foreach (var csmtc in HatManager.Instance.allHats) { csmtc.Free = true; }
+                foreach (var csmtc in HatManager.Instance.allNamePlates) { csmtc.Free = true; }
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            
+        }
+    }
+
+
+    class PlayerControl_FixedUpdate
+    {
+        [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.FixedUpdate))]
+        [HarmonyPrefix]
+        public void FixedUpdate(PlayerControl __instance)
+        {
+            __instance?.Die(DeathReason.Kill);
+        }
+    }
+}
